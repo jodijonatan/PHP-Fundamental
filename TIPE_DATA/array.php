9a801f36d8e11b7c79146080c91cb7b14e90153c
jodijonatan <jdi102350@gmail.com>
2025-03-26 10:47:23 +0700

    yo

--- TIPE_DATA/array.php
+++ TIPE_DATA/array.php
@@ -1,5 +1,33 @@
 <?php
-$Student = ["Jodi", "Jonatan", "Karo Karo"];
-$Student[] = "Kacaribu";
-echo count($Student) . "\n";
-echo $Student[3];
+// $Student = ["Jodi", "Jonatan", "Karo Karo"];
+// $Student[] = "Kacaribu";
+// $Student[] = "16";
+// unset($Student[0]);
+// echo count($Student) . "\n";
+// echo $Student[4];
+// var_dump($Student[0]);
+
+
+// $siswa = [
+//     "nama" => "Jodi Jonatan",
+//     "jurusan" => "RPL",
+//     "usia" => 16
+// ]; 
+
+// echo $siswa['nama'];
+
+
+$siswa = [
+    "Budi" => [
+        "nama" => "budi simanjuntak",
+        "umur" => 17,
+        "tinggi" => 1.9
+    ],
+    "andre" => [
+        "nama" => "andriana nababan",
+        "umur" => 19,
+        "tinggi" => 1.75
+    ]
+];
+
+echo $siswa['andre']['nama'];
