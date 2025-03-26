b33135bc9caf0c70c7aee5e418ee9ad36a043e97
jodijonatan <jdi102350@gmail.com>
2025-03-26 14:54:03 +0700

    array operator

---
+++ OPERATOR/OArray.php
@@ -0,0 +1,12 @@
+<?php
+
+$First = [
+    "firstName" => "Jodi"
+];
+
+$Last = [
+    "lastName" => "Jonatan"
+];
+
+$full = $First + $Last;
+var_dump($full);
---
+++ OPERATOR/aritmatika.php
@@ -0,0 +1,10 @@
+<?php
+
+$angka1 = 2;
+$angka2 = 5;
+
+// $pangkat = $angka2 % $angka1;
+// var_dump($pangkat);
+
+$angka1 **= $angka2;
+echo $angka1;
