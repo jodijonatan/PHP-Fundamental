60a400d359396e3d6b0c82f8525d47cb7ba81167
jodijonatan <jdi102350@gmail.com>
2025-04-08 15:47:39 +0700

    update return type

--- FUNCTION/returnType.php
+++ FUNCTION/returnType.php
@@ -4,5 +4,12 @@ function sum (int $first, int $second) : int {
     return $first + $second;
 }
 
+function sun (string $value) : string {
+    return $value;
+}
+
 $hasil = sum (12, 23);
-echo $hasil;
+echo $hasil . PHP_EOL;
+
+$hasil2 = sun ("124");
+echo $hasil2;
